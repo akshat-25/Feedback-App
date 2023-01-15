a095c469ce03b91644d0181758a76c5732bf61f7
Akshat Parakh <parakh.akshat25@gmail.com>
2023-01-15 20:53:52 +0530

    second commit

--- src/components/FeedbackLists.jsx
+++ src/components/FeedbackLists.jsx
@@ -1,22 +1,27 @@
 import FeedbackItem from "./FeedbackItem"
 import { useContext } from "react"
 import FeedbackContext from "../context/FeedbackContext"
-
+import Spinner from './shared/Spinner'
 
 function FeedbackLists() {
-    const {feedback} = useContext(FeedbackContext)
-    if(!feedback || feedback.length === 0){
+    const {feedback, isLoading} = useContext(FeedbackContext)
+    if(!isLoading && (!feedback || feedback.length === 0)){
         return <p>No Feedback Yet</p>
+
     }
-    return (
-    <div className='feedback-list'>
-        {feedback.map((item) => (
-        <
-         FeedbackItem key={item.id} 
-         item={item}
-        />
-    ))}
-    </div>
-  )
+    return isLoading ? (
+       <Spinner/>
+      ) : (
+        <div className='feedback-list'>
+       
+            {feedback.map((item) => (
+              
+                <FeedbackItem key={item.id} item={item} />
+             
+            ))}
+    
+        </div>
+      )
+    
 }
 export default FeedbackLists
